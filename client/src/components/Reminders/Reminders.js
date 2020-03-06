339e0d002b1d24c8402b8acf81761cf591fb5467
pmarinelli18 <pmarinelli18@gmail.com>
2020-03-05 23:01:16 -0500

    Added Reminder and appointment components

---
+++ client/src/components/Reminders/Reminders.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Reminders.css';
+
+const Reminders = () => {
+    return (
+        <div className = "Reminders">
+            this is a reminder
+
+        </div>
+    )
+};
+export default Reminders;
--- client/src/views/User/User.js
+++ client/src/views/User/User.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './User.css';
+import Reminders from '../../components/Reminders/Reminders';
+import Appointments from '../../components/Appointments/Appointments';
 
 function User() {
     return (
     <div className="App">
-    <header className="App-header">    
+    <header className="App-header">  
+        <Reminders />
+        <Appointments/>
     </header>
     
 </div>
