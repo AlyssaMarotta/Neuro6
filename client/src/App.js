887b7e37ccb17d9064c21b6d62aec5595e0b527e
Jason Fernandez <fernandezjason@users.noreply.github.com>
2020-03-09 17:36:30 -0400

    login2

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,32 @@
-import React, {useEffect} from 'react';
-import { Route, Switch, Redirect  } from 'react-router-dom';
-import Home from "./views/Home/Home";
-import NotFound from "./views/NotFound";
-import NavBar from "./components/Header/NavBar";
-import UserNavBar from "./components/UserHeader/UserNavBar";
+import React, { useEffect } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Home from './views/Home/Home';
+import NotFound from './views/NotFound';
+import NavBar from './components/Header/NavBar';
+import UserNavBar from './components/UserHeader/UserNavBar';
 import CreateUser from './views/CreateUser/CreateUser';
 import User from './views/User/User';
 import Login from './views/Login/Login';
 
 const App = () => {
-
-  useEffect(() => {
-
-    //sourced from https://medium.com/@maison.moa/setting-up-an-express-backend-server-for-create-react-app-bc7620b20a61
-    const callBackendAPI = async () => {
-      const response = await fetch('/Home', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: '{}'
-      });
-      const body = await response.json();
-  
-      if (response.status !== 200) {
-        throw Error(body.message) 
-      }
-      return body;
-    };
-
-    callBackendAPI()
-      .then(res => console.log(res))//this.setState({ data: res.express }))
-      .catch(err => console.log(err));
-  }, [])
-
-
   return (
     <div>
       <Switch>
-
-      <UserNavBar exact path="/User"/>
-        <NavBar exact path= ""/>
+        <UserNavBar exact path='/User' />
+        <NavBar exact path='' />
       </Switch>
       <Switch>
-        <Route exact path="/Home" component={Home} />
-        <Route exact path="/User" component={User} />
-        <Route exact path="/CreateUser" component={CreateUser} />4
-        <Route exact path="/Login" component={Login} />
-        <Route exact path="/">
-          <Redirect to="/Home" />
+        <Route exact path='/Home' component={Home} />
+        <Route exact path='/User' component={User} />
+        <Route exact path='/CreateUser' component={CreateUser} />4
+        <Route exact path='/Login' component={Login} />
+        <Route exact path='/'>
+          <Redirect to='/Home' />
         </Route>
-        <Route component={NotFound}/>
+        <Route component={NotFound} />
       </Switch>
     </div>
   );
-}
+};
 
 export default App;
--- client/src/views/CreateUser/CreateUser.js
+++ client/src/views/CreateUser/CreateUser.js
@@ -3,48 +3,36 @@ import { Link } from 'react-router-dom';
 import './CreateUser.css';
 
 function CreateUser() {
-    return (
-    <div className="App">
-    <header className="App-header">    
-    <div className="forum">
-         <p>
-            <input
-                type="text"
-                placeholder = "First Name"
-            />
-        </p>
-        <p>
-            <input
-                type="text"
-                placeholder = "Last Name"
-            />
-        </p>
-        <p>
-            <input
-                type="text"
-                placeholder = "Email"
-            />
-        </p>
-        <p>
-            <input
-                type="text"
-                placeholder = "Username"
-            />
-        </p>
-        <p>
-            <input
-                type="text"
-                placeholder = "Password"
-            />
-        </p>
-        <p>
-            <Link className = "nav-link" to='/User'>Enter</Link>
-        </p>
-    </div>    
-    </header>
-    
-</div>
-);
+
+   
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <div className='forum'>
+          <p>
+            <input type='text' placeholder='First Name' />
+          </p>
+          <p>
+            <input type='text' placeholder='Last Name' />
+          </p>
+          <p>
+            <input type='text' placeholder='Email' />
+          </p>
+          <p>
+            <input type='text' placeholder='Username' />
+          </p>
+          <p>
+            <input type='text' placeholder='Password' />
+          </p>
+          <p>
+            <Link className='nav-link' to='/User'>
+              Enter
+            </Link>
+          </p>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default CreateUser;
