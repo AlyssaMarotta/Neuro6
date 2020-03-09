1ecc9b1ab2338c3c72c58344b571397c4a199a59
Jason Fernandez <fernandezjason@users.noreply.github.com>
2020-03-09 16:35:35 -0400

    login1

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home";
 import NotFound from "./views/NotFound";
 import NavBar from "./components/Header/NavBar";
 
 const App = () => {
+
+  useEffect(() => {
+
+    //sourced from https://medium.com/@maison.moa/setting-up-an-express-backend-server-for-create-react-app-bc7620b20a61
+    const callBackendAPI = async () => {
+      const response = await fetch('/Home', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: '{}'
+      });
+      const body = await response.json();
+  
+      if (response.status !== 200) {
+        throw Error(body.message) 
+      }
+      return body;
+    };
+
+    callBackendAPI()
+      .then(res => console.log(res))//this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }, [])
+
+
   return (
     <div>
       <NavBar />
--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express = require('./config/express.js')
- 
+
 // Use env port or default
 const port = process.env.PORT || 5000;
 
 const app = express.init()
+
+app.post('/Home', (req, res) => {
+    res.send({express : 'Express backend is connected to react'})
+    
+})
+
+
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
